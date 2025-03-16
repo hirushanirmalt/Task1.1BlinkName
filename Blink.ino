0fc1f3d6f3c78e224ce653f6c1c16b4c5d5c0c7e
hirushanirmalt <141208448+hirushanirmalt@users.noreply.github.com>
2025-03-16 17:52:09 +1100

    Update Blink.ino

--- Blink.ino
+++ Blink.ino
@@ -1,46 +1,40 @@
-
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
-}
 
-// the loop function runs over and over again
-void loop() {
-  //The First name should be DON
-  //Letter D
+}
+  void dash(){
   digitalWrite(LED_BUILTIN, HIGH);  
   delay(1000);                      //Delay for a dash
   digitalWrite(LED_BUILTIN, LOW);   
-  delay(700);                       // space between dots and a dashes
+  delay(700);      
+
+  }
+void dot(){
   digitalWrite(LED_BUILTIN, HIGH);
   delay(500);                       // delay for a dot
   digitalWrite(LED_BUILTIN, LOW);
   delay(700);
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(500);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(1200);                      // delay between letters
-  //Letter O
-  digitalWrite(LED_BUILTIN, HIGH);  
-  delay(1000);       
-  digitalWrite(LED_BUILTIN, LOW);   
-  delay(700); 
-  digitalWrite(LED_BUILTIN, HIGH);  
-  delay(1000);  
-  digitalWrite(LED_BUILTIN, LOW);   
-  delay(700); 
-  digitalWrite(LED_BUILTIN, HIGH);  
-  delay(1000);  
-  digitalWrite(LED_BUILTIN, LOW);   
-  delay(1200); 
-  //Letter N 
-  digitalWrite(LED_BUILTIN, HIGH);  
-  delay(1000);       
-  digitalWrite(LED_BUILTIN, LOW);   
-  delay(700); 
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(500);
-
-  digitalWrite(LED_BUILTIN, LOW); //Delay before the next loop
-  delay(2000);
 }
+void morsecode(String massege){
+  
+  int i = 0;
+  //loop runs untill the end of the text input
+  while (i<massege.length()){
+  char c= massege[i];
+  switch (c)
+  { //This part can be designed to contain all the letters of the alphebet
+    case 'D': dash();dot();dot(); break;
+    case 'O' : dash(); dash(); dash(); break;
+    case 'N' : dash(); dot(); break;
+  }
+  
+  i++;}
+}
+// the loop function runs over and over again
+void loop() {
+ // morse code will be shown for whatever word that input here
+  morsecode("DON");
+  // delay between massege repeat 
+  delay(2000);
+  
